2d622dabf9f59a4ac64954c16a9ec91ef76bddfd
MrBenel <Balalebanel@gmail.com>
2026-02-25 02:50:26 +0100

    Update next.config.ts for GitHub Pages
    Add configuration for GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export', // Indispensable pour GitHub Pages
+  basePath: '/portfolio', // Remplace par le nom exact de ton dépôt
+  images: {
+    unoptimized: true, // GitHub Pages ne supporte pas l'optimisation d'image native de Next
+  },
 };
 
 export default nextConfig;
